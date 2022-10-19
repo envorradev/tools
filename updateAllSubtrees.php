7c487022aa417d8effde088b87c85f548d28d02f
Sam Taylor <15961687+samueljtaylor@users.noreply.github.com>
2022-10-19 12:53:53 -0400

    wip

---
+++ updateAllSubtrees.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+require __DIR__.'/vendor/autoload.php';
+
+use Envorra\GitHelper\Routines\UpdateAllSubtreesRoutine;
+
+$routine = new UpdateAllSubtreesRoutine(
+    repositoryBaseUrl: 'git@github.com:envorradev/',
+    prefixMap: [
+        'tool-arrays' => 'src/Arrays',
+        'tool-common' => 'src/Common',
+        'tool-composer' => 'src/Composer',
+        'tool-filesystem' => 'src/Filesystem',
+        'tool-flags' => 'src/Flags',
+        'tool-json' => 'src/Json',
+    ]
+);
+
+$routine->run();
